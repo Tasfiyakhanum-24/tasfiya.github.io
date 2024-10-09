2d2a2d667b8626af2b72c175d0da6bd98a91a043
Tasfiyakhanum-24 <tasfiyakhanum7@gmail.com>
2024-10-09 11:43:18 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+// Open default email client
+function openEmail() {
+    window.location.href = "mailto:tasfiyakhanum7@gmail.com";
+}
+
+// Open LinkedIn profile
+function openLinkedIn() {
+    window.open("https://www.linkedin.com/in/tasfiya-khanum-6028b6222", "_blank");
+}
